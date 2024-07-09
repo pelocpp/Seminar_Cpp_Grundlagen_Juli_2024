d23e4f65eaee983aad42e7824784e258a9c455e8
Peter Loos <peter.loos@gmx.de>
2024-07-09 10:07:47 +0200

    Add project files.

---
+++ Seminar_Cpp_Grundlagen_Juli_2024/Program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()  // main muss so sein.
+{
+    int a,  b, c;
+
+    a = 1;
+    b = 2;
+    c = a + b;
+
+    std::cout << "Hello World" << std::endl;  // C++
+    // printf
+
+    return 0;
+}
